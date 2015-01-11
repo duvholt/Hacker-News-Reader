702aa598bc73716b5a985dc709eecbd1fa218e9b
Christian Duvholt <cxhristian@gmail.com>
2015-01-11 01:12:36 +0100

    Fixed comments

--- api/backends/firebase.py
+++ api/backends/firebase.py
@@ -65,21 +65,18 @@ class FirebaseAPI(BaseAPI):
     def comments(self, itemid, cache_minutes):
         self.itemid = itemid
         comments = self.fetch.comments(self.itemid)
-        if 'message' in comments:
-            raise utils.ShowAlert(comments['message'])
-        elif 'error' in comments:
-            raise utils.ShowAlert(comments['error'])
         if comments['type'] in ['story', 'poll']:
             self.story_id = self.itemid
             self.story_info(comments)
             count = 0
-            for comment in comments['children']:
+            for comment_id in comments['kids']:
                 count += 1
+                comment = self.fetch.comments(comment_id)
                 count += self.traverse_comments(comment)
             self.story.comments = count
             if comments['type'] == 'poll':
                 self.story.poll = True
-                self.poll_info(comments['options'])
+                self.poll_info(comments['parts'])
             self.story.save()
             # models.HNCommentsCache(id=self.itemid, time=timezone.now()).save()
         elif comments['type'] == 'comment':
@@ -88,9 +85,7 @@ class FirebaseAPI(BaseAPI):
                 comment_object = models.HNComments.objects.get(id=self.itemid)
                 self.story_id = comment_object.story_id
             except models.HNComments.DoesNotExist:
-                # Story is not saved, so let's fetch the entire story instead of just the comment tree.
-                # TODO: Check if this is too slow
-                return self.comments(comments['story_id'], cache_minutes)
+                self.story_id = None
             if self.story_id:
                 try:
                     self.story = models.Stories.objects.get(id=self.story_id)
@@ -103,25 +98,28 @@ class FirebaseAPI(BaseAPI):
 
     def traverse_comments(self, comment, parent_object=None):
         if not parent_object and not self.story:
-            parent_object = self.parent(comment['parent_id'])
-        if 'author' not in comment:
-            # Dead comment with no info
-            return 0
+            parent_object = self.parent(comment['parent'])
         HNComment = models.HNComments()
+        if 'deleted' in comment:
+            return 0
+        if 'dead' in comment:
+            HNComment.dead = comment['dead']
         HNComment.id = comment['id']
-        HNComment.username = comment['author']
+        HNComment.username = comment['by']
         HNComment.text = utils.html2markup(comment['text'])
         HNComment.story_id = self.story_id
         HNComment.parent = parent_object
         tz = get_localzone()
-        HNComment.time = self.dateformat(comment['created_at'])
+        HNComment.time = self.dateformat(comment['time'])
         HNComment.cache = timezone.now()
         HNComment.save()
         # models.HNCommentsCache(id=HNComment.id, time=timezone.now()).save()
         count = 0
-        for comment_child in comment['children']:
-            count += 1
-            count += self.traverse_comments(comment_child, HNComment)
+        if 'kids' in comment:
+            for comment_id in comment['kids']:
+                count += 1
+                comment_child = self.fetch.comments(comment_id)
+                count += self.traverse_comments(comment_child, HNComment)
         return count
 
     def story_info(self, story):
@@ -132,24 +130,25 @@ class FirebaseAPI(BaseAPI):
         if story['text']:
             self.story.selfpost = True
             self.story.selfpost_text = utils.html2markup(story['text'])
-        self.story.username = story['author']
-        self.story.url = "" if story['url'] is None else story['url']
-        self.story.time = self.dateformat(story['created_at'])
-        self.story.score = story['points']
+        self.story.username = story['by']
+        self.story.url = "" if 'url' not in story else story['url']
+        self.story.time = self.dateformat(story['time'])
+        self.story.score = story['score']
 
     def poll_info(self, polls):
-        for option in polls:
-            poll = models.Poll(id=option['id'])
-            poll.time = self.dateformat(option['created_at_i'])
-            poll.name = utils.html2markup(option['text'])
-            poll.score = option['points']
-            poll.story_id = option['parent_id']
+        for part in polls:
+            part = self.fetch.comments(part)
+            poll = models.Poll(id=part['id'])
+            poll.time = self.dateformat(part['time'])
+            poll.name = utils.html2markup(part['text'])
+            poll.score = part['score']
+            poll.story_id = part['parent']
             poll.save()
 
     def userpage(self, username):
         userpage = self.fetch.userpage(username)
         if not userpage:
-            # User doesn't exist
+            # User doesn't e
             return
         user = models.UserInfo()
         user.username = userpage['id']
@@ -171,7 +170,7 @@ class FirebaseAPI(BaseAPI):
             return datetime.datetime(*time.strptime(datecreate, '%Y-%m-%dT%H:%M:%S.000Z')[0:6], tzinfo=tz)
         # Unix time
         elif type(datecreate) is int:
-            return datetime.datetime.fromtimestamp(datecreate)
+            return datetime.datetime.fromtimestamp(datecreate).replace(tzinfo=tz)
 
 
 class FirebaseFetch(BaseFetch):
