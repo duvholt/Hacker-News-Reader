25f4072f30aa6a8b6b3c9b0db5fdcb746e08b20f
Christian Duvholt <cxhristian@gmail.com>
2015-01-12 09:16:26 +0100

    Default to algolia backend because the firebase implementation is pretty slow at the moment

--- reader/settings.py
+++ reader/settings.py
@@ -14,7 +14,7 @@ ADMINS = (
 DOMAIN_URL = 'hn.cxhristian.com'
 
 # HN API Backend
-API_BACKEND = 'firebase'
+API_BACKEND = 'algolia'
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
