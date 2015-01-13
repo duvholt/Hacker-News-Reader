602f860a25902c51edc1b384e67d69968cfb172d
Christian Duvholt <cxhristian@gmail.com>
2015-01-13 08:27:29 +0100

    Oooops

--- api/__init__.py
+++ api/__init__.py
@@ -10,7 +10,7 @@ if API_BACKEND == 'algolia':
     API = algolia.AlgoliaAPI()
 elif API_BACKEND == 'builtin':
     API = builtin.BuiltinAPI()
-if API_BACKEND == 'firebase':
+elif API_BACKEND == 'firebase':
     API = firebase.FirebaseAPI()
 else:
     API = builtin.BuiltinAPI()
